2d53dd67ce56d067c460c26e6f5a212473633a2b
juliop110 <juliootero7@gmail.com>
2021-09-22 18:11:26 -0300

    projeto Srping criado

---
+++ src/main/java/br/com/otero/personalcredit/PersonalcreditApplication.java
@@ -0,0 +1,13 @@
+package br.com.otero.personalcredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalcreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalcreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/otero/personalcredit/PersonalcreditApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.otero.personalcredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalcreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
